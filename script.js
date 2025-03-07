d2331676acc541cd95719028d6d51fc5656bff5f
Alex-Pfeifer <pfeifer.alex@hotmail.de>
2025-03-07 22:05:35 +0100

    task- function filterPrimes is ready

---
+++ script.js
@@ -0,0 +1,26 @@
+let arrNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 17, 19];
+
+function filterPrimes(arr) {
+    const primeNumbers = [];
+    for (let i = 0; i < arr.length; i++) {
+        let n = arr[i];
+        let isPrime = true;
+
+        if (n < 2) {
+            isPrime = false;
+    } else {
+        for (let j = 2; j * j <= n; j++) {
+            if (n % j === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+    if (isPrime) {
+        primeNumbers[primeNumbers.length] = n;
+    }
+}
+return primeNumbers;
+}
+
+console.log(filterPrimes(arrNumber));
